e39c7dfdcdfa273cf879cb156a77d6889b52f7b4
Sergey Chernikov <schernikov@ptsecurity.com>
2025-05-18 21:03:14 +0300

    Fix doc

--- include/humble/concurrent/rcu.hpp
+++ include/humble/concurrent/rcu.hpp
@@ -1,5 +1,5 @@
-#ifndef LIBHUMBLE_CPP_RCU_H_
-#define LIBHUMBLE_CPP_RCU_H_
+#ifndef LIBHUMBLE_CPP_CONCURRENT_RCU_H_
+#define LIBHUMBLE_CPP_CONCURRENT_RCU_H_
 
 #include <cassert>
 #include <chrono>
@@ -27,13 +27,13 @@ public:
 private:
     using AtomicConstResourcePointer    = std::atomic<ConstPointer>;
 
-    static_assert(AtomicConstPointer::is_always_lock_free);
+    static_assert(AtomicConstResourcePointer::is_always_lock_free);
 
-    OwningResourcePointer           resource_{};
-    OwningResourcePointer           resource_to_remove_{};
-    AtomicConstResourcePointer      cached_resource_ptr_{};
-    size_t                          active_readers_{};          ///< A number of readers online
-    std::atomic<size_t>             readers_upd_in_progress_{}; ///< A number of readers which haven't fetched the new pointer yet
+    OwningResourcePointer           resource_{};                ///< A current active resourse.
+    OwningResourcePointer           outdated_resource_{};       ///< A resourse is about to removed, but potentially in use.
+    AtomicConstResourcePointer      cached_resource_ptr_{};     ///< An atomic pointer to `resource_`.
+    size_t                          active_readers_{};          ///< A number of readers online.
+    std::atomic<size_t>             readers_upd_in_progress_{}; ///< A number of readers which haven't fetched the new pointer yet.
     std::mutex                      mtx_;
 
     void notify_updated() noexcept
@@ -74,6 +74,7 @@ public:
     RcuContext(const RcuContext &) = delete;
     RcuContext & operator=(const RcuContext &) = delete;
 
+    /// @warning The context MUST have no active readers.
     ~RcuContext()
     {
         assert(!in_progress());
@@ -82,9 +83,12 @@ public:
 
     auto readers_in_progress() const noexcept   { return readers_upd_in_progress_.load(std::memory_order_acquire); }
 
-    /// @brief Check if there is any reader having not fetched the new pointer yet (an update is in progress)
+    /// @brief Checks if there is at least one active reader that haven't fetched the new pointer yet (i.e. an update is in progress).
     bool in_progress() const noexcept           { return !!readers_in_progress(); }
 
+    /// @brief Assigns a new resourse pointer.
+    /// @param p A resource pointer.
+    /// @warning There MUST be no any operation in progress.
     void update(OwningResourcePointer p) noexcept
     {
         assert(!in_progress()); // no readers waiting for update
@@ -93,22 +97,24 @@ public:
         std::lock_guard lock{mtx_};
         readers_upd_in_progress_.store(active_readers_, std::memory_order_release);
 
-        resource_to_remove_ = std::exchange(resource_, std::move(p)); // update stored owning pointer
+        outdated_resource_ = std::exchange(resource_, std::move(p)); // update stored owning pointer
         cached_resource_ptr_.store(value_ptr_.get(), std::memory_order_release); // make visible a new pointer for readers
     }
 
-    /// @brief Checks if an update in progress and releases the old resource if not.
-    /// @return `true` if there are no readers that haven't updated their caches, otherwise - `false`
+    /// @brief If there is no update in progress - releases the old resource.
+    /// @return `true` - if the operation succeeded, otherwise - `false`.
     bool check_update_complete() noexcept
     {
         if (!in_progress())
         {
-            resource_to_remove_.reset();
+            outdated_resource_.reset();
             return true;
         }
         return false;
     }
 
+    /// @brief Blocks the current thread and wait for update completion.
+    /// @note Exploits a spin-lock.
     void wait_update_complete() noexcept
     {
         static constexpr unsigned kBusyCycles{256};
@@ -139,8 +145,12 @@ class RcuReader
     bool                    active_{};
 
 public:
+    /// @brief Creates an invalid (empty) RCU resource reader.
     RcuReader() = default;
 
+    /// @brief Creates a new valid RCU resource reader.
+    /// @param ctx A reference to the parent RCU context.
+    /// @param active If the reader is activated at the construction.
     explicit RcuReader(Context &ctx, bool active = true) noexcept
         : ctx_{&ctx}
         , cached_resource_ptr_{active ? ctx_->activate_reader() : nullptr}
@@ -151,6 +161,7 @@ public:
     RcuReader(const RcuReader & ) = delete;
     RcuReader & operator=(const RcuReader & ) = delete;
 
+    /// @brief Moves the resource from another reader leaving it invalid.
     RcuReader(RcuReader &&other) noexcept
         : ctx_{std::exchange(other->ctx_, nullptr)}
         , cached_resource_ptr_{std::exchange(other->cached_resource_ptr_, nullptr)}
@@ -158,6 +169,7 @@ public:
     {
     }
 
+    /// @brief Moves the resource from another reader leaving it invalid.
     RcuReader & operator=(RcuReader &&other) noexcept
     {
         ctx_                    = std::exchange(other->ctx_, nullptr);
@@ -166,34 +178,45 @@ public:
         return *this;
     }
 
+    /// @note Also deregisters the reader if it's valid and active.
     ~RcuReader()
     {
         if (is_valid() && is_active())
             ctx_->deactivate_reader(cached_resource_ptr_);
     }
 
+    /// @brief Checks if the object is bound to any parent RCU context.
+    /// @note It's the only operation permited on an invalid object (except for constructors and operators).
     bool is_valid() const noexcept  { return !!ctx_; }
+
+    /// @brief Checks if the object registered as active.
     bool is_active() const noexcept { assert(is_valid()); return active_; }
 
     explicit operator bool() const noexcept { return is_valid(); }
 
+    /// @brief Gets a resource pointer fetch at the last `update()`.
     ConstResourcePointer get_resource() const noexcept  { assert(is_active()); return cached_resource_ptr_; }
 
+    /// @brief Registers the object as an active in the parent or does nothing if it's already active.
     void activate() noexcept
     {
-        assert(!is_active());
+        if (is_active()) return;
         cached_resource_ptr_    = ctx_->activate_reader();
         active_                 = true;
     }
 
+    /// @brief Deregisters the object in the parent.
     void deactivate() noexcept
     {
-        assert(is_active());
+        if (!is_active()) return;
         ctx_->deactivate_reader(cached_resource_ptr_);
         cached_resource_ptr_    = nullptr;
         active_                 = false;
     }
 
+    /// @brief Fetchs a new resourse pointer from the parent context. Do nothing if the resource hasn't changed.
+    /// @details Compares pointers to determine if the resourse has changed.
+    /// @warning The object MUST be valid and active.
     void update() noexcept
     {
         assert(is_active());
@@ -203,4 +226,4 @@ public:
 
 } // namespace hmbl::concurrent
 
-#endif // LIBHUMBLE_CPP_RCU_H_
+#endif // LIBHUMBLE_CPP_CONCURRENT_RCU_H_
